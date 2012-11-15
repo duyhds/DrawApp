8b5d64cce37d8ddb887a9f5a5a824f4b19a231ab
duyhds <emosk8erboi@hotmail.co.uk>
2012-11-15 20:18:56 +0000

    Update examples/turtle.c

--- examples/turtle.c
+++ examples/turtle.c
@@ -6,12 +6,16 @@ int main(void)
 	  int i = 0;
 	  
 	  for( i = 0; i < 20; i++){
+			setColour(blue);
 			turtleForward(x);
 			x = x + 5;
+			setColour(yellow);
 			turtleRight(x);
 			x = x + 5; 
+			setColour(green);
 			turtleBackward(x);
 			x = x + 5;
+			setColour(cyan);
 			turtleLeft(x);
 			x = x + 5;
 	  }
